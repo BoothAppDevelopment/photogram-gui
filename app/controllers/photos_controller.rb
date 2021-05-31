574a03205a3ff1478815ee6d5de6ef7c3e219a47
Hammad-Rahman <hammad.rahman@chicagobooth.edu>
2021-05-31 01:52:31 +0000

    photos index minus form done

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,18 @@
+class PhotosController < ApplicationController
+
+  def index
+    @list_of_photos = Photo.all
+
+    render({ :template => "photo_templates/index.html.erb"})
+  end
+
+
+  def show
+
+    render({ :template => "photo_templates/show.html.erb"})
+  end
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
-get("/users", {:controller => "users", :action => "index"})
-get("/users/:path_username", {:controller => "users", :action => "show"})
+  get("/", {:controller => "users", :action => "index"})
 
+  # Users
+  get("/users", {:controller => "users", :action => "index"})
+  get("/users/:path_username", {:controller => "users", :action => "show"})
 
+  # Photos
+  get("/photos", {:controller => "photos", :action => "index"})
+  get("/users/:path_photo_id", {:controller => "photos", :action => "show"})
 end
